afdde2ef4a4f07196a716e3b55b699599cac06f9
Ben Pickles <spideryoung@gmail.com>
2015-05-12 18:19:42 +0100

    Don't need these.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,8 +10,5 @@ require 'bundler/setup'
 require 'static_association'
 
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
-  config.run_all_when_everything_filtered = true
-  config.filter_run :focus
   config.order = 'random'
 end
